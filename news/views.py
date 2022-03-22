f3ae1e3ec79841f4c89a0e33dbe2ad43fd4efc97
saretotracy <tracysareto@gmail.com>
2022-03-22 16:11:54 +0300

    Add error handler

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
+from django.http  import HttpResponse,Http404
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
