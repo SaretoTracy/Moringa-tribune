e46532ba86c007957089c20dfc73e77b1fb5a444
saretotracy <tracysareto@gmail.com>
2022-03-23 08:36:08 +0300

    create the Article model clas and perform one to many relationship

--- news/models.py
+++ news/models.py
@@ -20,3 +20,7 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
