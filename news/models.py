549ebee5a397f7c56a0b8531ee9ed4324145b3d9
saretotracy <tracysareto@gmail.com>
2022-03-23 08:11:49 +0300

    creating out first model.

--- news/models.py
+++ news/models.py
@@ -7,3 +7,8 @@ class UrlDateConverter:
     format='%Y-%m-%d'
     def to_python(self,value):
         return dt.strptime(value,self.format)
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
