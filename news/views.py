386c5ff044755d90471c84cb3f16041314e32c6d
saretotracy <tracysareto@gmail.com>
2022-03-23 07:22:57 +0300

    perform Template Inheritance

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
 from django.shortcuts import render
@@ -18,7 +18,7 @@ def news_of_day(request):
             </body>
         </html>
             '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 def convert_dates(dates):
 
@@ -32,15 +32,19 @@ def convert_dates(dates):
     return day
 
 def past_days_news(request,past_date):
+
+    try:
+
         # Converts data from the string Url
         date = past_date
 
         day = convert_dates(past_date)
-        html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-                '''
-        return HttpResponse(html)
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
