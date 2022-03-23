8949ea103faf70275a9a8f37f6e0b779bd89fa66
saretotracy <tracysareto@gmail.com>
2022-03-23 08:34:22 +0300

    create tag model

--- news/models.py
+++ news/models.py
@@ -12,3 +12,11 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
