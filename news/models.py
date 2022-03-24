eb58567d73f2fda75705cf2c512a459111310e4c
saretotracy <tracysareto@gmail.com>
2022-03-24 07:34:14 +0300

    update view and template files

--- news/models.py
+++ news/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from datetime import datetime as dt
+import datetime as dt
 
 # Create your models here.
 class UrlDateConverter:
     regex='\d{4}-\d{2}-\d{2}'
     format='%Y-%m-%d'
     def to_python(self,value):
-        return dt.strptime(value,self.format)
+        return dt.datetime.strptime(value,self.format)
 
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Article
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
 # Create your views here.
@@ -11,14 +12,9 @@ def news_of_day(request):
     date = dt.date.today()
      # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return render(request, 'all-news/today-news.html', {"date": date,})
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
 
 def convert_dates(dates):
 
@@ -47,5 +43,5 @@ def past_days_news(request,past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
-
+        news = Article.days_news(date)
+        return render(request, 'all-news/past-news.html',{"date": date,"news":news})
