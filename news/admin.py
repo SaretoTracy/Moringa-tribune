fa351a00c43aa247797504c69d7054acfdbbce43
saretotracy <tracysareto@gmail.com>
2022-04-01 12:11:40 +0300

    remove editor model

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
-from .models import Editor,Article,tags
+from .models import Article,tags
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tags',)# filter_horizontal property that allows ordering of many to many fields
-admin.site.register(Editor)
 admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
--- news/models.py
+++ news/models.py
@@ -8,19 +8,7 @@ class UrlDateConverter:
     def to_python(self,value):
         return dt.datetime.strptime(value,self.format)
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length = 10,blank =True)
 
-    def __str__(self):
-        return self.first_name
-
-    def save_editor(self):
-        self.save()
-    def delete_editor(self):
-        self.delete()
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
@@ -29,7 +17,7 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    editor = models.CharField(max_length =60)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
     article_image = models.ImageField(upload_to = 'articles/',default="")
