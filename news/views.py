4a8290449f9c40a4cb8ff426ad5987d90d1c3810
saretotracy <tracysareto@gmail.com>
2022-04-06 10:51:56 +0300

    create a new view function that will asynchronously save the user to the database and send the welcome email.

--- news/views.py
+++ news/views.py
@@ -17,24 +17,22 @@ def news_of_day(request):
      # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
     news = Article.todays_news()
-    if request.method == 'POST':
-        form = NewsLetterForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['your_name']
-            email = form.cleaned_data['email']
-
-            recipient = NewsLetterRecipients(name = name,email =email)
-            recipient.save()
-            send_welcome_email(name,email)
-
-            HttpResponseRedirect('news_today')
-    else:
-       form = NewsLetterForm()
+    form = NewsLetterForm()
 
 
     return render(request, 'all-news/today-news.html',{"date": date, "news": news, "letterForm":form})
 
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
 
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
+
+    
 def convert_dates(dates):
 
     # Function that gets the weekday number for the date.
